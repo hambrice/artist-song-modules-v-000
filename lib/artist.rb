5fb6d0965d6e219ee68fefde2470c7ca4e8925e5
Brad Hambrice <hambrice@uab.edu>
2018-01-10 08:19:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,8 +39,8 @@ extend Findable
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 
 end
